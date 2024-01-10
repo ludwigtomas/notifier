5435eeea1110d8ce7a411695926e904f68ab4066
mayhemfn22 <mayhemfn22@gmail.com>
2024-01-10 18:48:53 +0100

    client change

--- resources/js/Pages/Clients/Create.jsx
+++ resources/js/Pages/Clients/Create.jsx
@@ -1,20 +1,41 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import TextInput from "@/Components/TextInput";
+import InputLabel from "@/Components/InputLabel";
+import InputError from "@/Components/InputError";
+import { useForm } from "@inertiajs/react";
+import PrimaryButton from "@/Components/PrimaryButton";
 
 export default function Dashboard({ auth }) {
+    const { data, setData, post, processing, errors } = useForm({
+        first_name: "",
+        last_name: "",
+        email: "",
+        phone: "",
+        ico: "",
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        post(route("clients.store"));
+    };
+
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Dashboard
+                </h2>
+            }
         >
             <Head title="Dashboard" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                clients
-                        </div>
+                        <div className="p-6 text-gray-900">clients</div>
                     </div>
                 </div>
             </div>
