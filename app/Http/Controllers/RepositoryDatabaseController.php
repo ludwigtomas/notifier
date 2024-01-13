008d4241096ac3f5222a60fafdac130006bb3958
BubakVOE <ludwigtom@seznam.cz>
2024-01-13 19:25:36 +0100

    update endpoint for database backup

--- app/Http/Controllers/RepositoryDatabaseController.php
+++ app/Http/Controllers/RepositoryDatabaseController.php
@@ -2,13 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreDatabaseRequest;
 use App\Models\Repository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class RepositoryDatabaseController extends Controller
 {
     public function store(Request $request, Repository $repository)
     {
-        return $repository;
+        try {
+            // $path = Storage::path($repository->slug . '/databases');
+
+            // if (!Storage::exists($path)) {
+            //     Storage::makeDirectory($path);
+            // }
+
+            $file = $request->file('file');
+
+            if ($repository->database_backups()->where('name', $file->getClientOriginalName())->exists()) {
+                return response()->json([
+                    'message' => 'Database already exists',
+                    'error' => 'Database already exists',
+                ], 409);
+            }
+
+            $repository->database_backups()->create([
+                'name' => $file->getClientOriginalName(),
+                'size' => $file->getSize() / 1000,
+            ]);
+
+            $file->storeAs($repository->slug . '/databases', $file->getClientOriginalName());
+
+            return response()->json([
+                'message' => 'Database uploaded successfully'
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Database upload failed',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
     }
 }
--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -18,5 +18,29 @@ class AdminSeeder extends Seeder
             'email' => 'admin@admin.com',
             'password' => bcrypt('secret123'),
         ]);
+        
+        // if (env('APP_ENV') === 'local') {
+        //     User::create([
+        //         'name' => 'Admin',
+        //         'email' => 'admin@admin.com',
+        //         'password' => bcrypt('secret123'),
+        //     ]);
+        // }
+
+        // if (env('APP_ENV') === 'production') {
+        //     User::create([
+        //         'name' => 'Admin',
+        //         'email' => 'admin@admin.com',
+        //         'password' => bcrypt('secret123'),
+        //     ]);
+
+        //     User::create([
+        //         'name' => 'Ludwig Tomas',
+        //         'is_admin' => 1,
+        //         'email' => 'ludwigtom@seznam.cz',
+        //         'email_verified_at' => now(),
+        //         'password' => '$2y$10$L3BkZ54/462S3LaTIhatauhdKsGBg5/7JivXfMY0mQo5qq94GfBMO',
+        //     ]);
+        // }
     }
 }
