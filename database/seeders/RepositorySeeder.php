267623c6758cb9d19edcbb638bd48bbffd160f69
BubakVOE <ludwigtom@seznam.cz>
2024-01-13 17:54:43 +0100

    fix DatabaseSeeder

--- database/seeders/RepositorySeeder.php
+++ database/seeders/RepositorySeeder.php
@@ -22,44 +22,6 @@ class RepositorySeeder extends Seeder
 
         GitlabService::getUserID($gitlab);
         GitlabService::downloadAvatar($gitlab);
-
-        $client = new GuzzleClient([
-            "base_uri" => "https://gitlab.com/api/v4/",
-        ]);
-
-        $response = $client->request('GET', 'groups/64297613/projects?order_by=updated_at&sort=desc', [
-            'headers' => [
-                'Authorization' => 'Bearer ' . $gitlab->api_token,
-            ],
-        ]);
-
-        $repositories = json_decode($response->getBody()->getContents());
-
-        foreach ($repositories as $repository_api) {
-
-            $repository = Repository::where('id', $repository_api->id)->first();
-
-            if ($repository) {
-                $repository->update([
-                    'name' => $repository_api->name,
-                    'slug' => Str::slug($repository_api->name),
-                    'repository_url' => $repository_api->web_url,
-
-                    'last_activity_at' => Carbon::parse($repository_api->last_activity_at),
-                    'repository_created_at' => Carbon::parse($repository_api->created_at),
-                ]);
-            } else {
-                $gitlab->repositories()->create([
-                    'id' => $repository_api->id,
-                    'name' => $repository_api->name,
-                    'slug' => Str::slug($repository_api->name),
-                    'repository_url' => $repository_api->web_url,
-
-                    'last_activity_at' => Carbon::parse($repository_api->last_activity_at),
-                    'repository_created_at' => Carbon::parse($repository_api->created_at),
-                ]);
-            }
-        }
-        // GitlabService::getRepositories($gitlab);
+        GitlabService::getRepositories($gitlab);
     }
 }
