a7c2084201ec26621d2754d5096fb1775caeeaef
BubakVOE <ludwigtom@seznam.cz>
2024-01-14 00:05:52 +0100

    test

--- app/Http/Controllers/RepositoryDatabaseController.php
+++ app/Http/Controllers/RepositoryDatabaseController.php
@@ -17,22 +17,33 @@ class RepositoryDatabaseController extends Controller
         $backupFile = $request->file('backup_file');
 
         // Kontrola, zda byl soubor úspěšně nahrán
-        if ($backupFile->isValid()) {
 
-            $repository->database_backups()->create([
-                'name' => $backupFile->getClientOriginalName(),
-                'size' => $backupFile->getSize() / 1000,
-            ]);
+        try {
+            if ($backupFile->isValid()) {
 
-            // Přesunutí souboru do umístění podle vašich potřeb
-            $path = $backupFile->storeAs('backup', 'backup-2024-12-01');
+                $repository->database_backups()->create([
+                    'name' => $backupFile->getClientOriginalName(),
+                    'size' => $backupFile->getSize() / 1000,
+                ]);
 
-            // Zde můžete dále zpracovávat cestu nebo provádět další akce
-            return response()->json(['message' => 'Soubor byl úspěšně nahrán.']);
-        } else {
-            // Soubor nebyl nahrán úspěšně
-            return response()->json(['error' => 'Chyba při nahrávání souboru.'], 400);
+                // Přesunutí souboru do umístění podle vašich potřeb
+                $path = $backupFile->storeAs('backup', 'backup-2024-12-01');
+
+                // Zde můžete dále zpracovávat cestu nebo provádět další akce
+                return response()->json(['message' => 'Soubor byl úspěšně nahrán.']);
+            } else {
+                // Soubor nebyl nahrán úspěšně
+                return response()->json(['error' => 'Chyba při nahrávání souboru.'], 400);
+            }
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Database upload failed',
+                'error' => $th->getMessage(),
+            ], 500);
         }
+
+
+
 
         // try {
         //     $file = $request->file('file');
