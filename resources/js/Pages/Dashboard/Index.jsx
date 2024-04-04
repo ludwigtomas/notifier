dac2ffc01890b48a3d9f256a94811b69f039d491
BubakVOE <ludwigtom@seznam.cz>
2024-04-04 02:13:23 +0200

    test automation

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -13,14 +13,6 @@ export default function Dashboard({ auth }) {
                     >
                         Dashboard
                     </Link>
-
-                    <Link
-                        className="font-semibold text-lg leading-tight"
-                        href={route('dashboard.index')}
-                    >
-                        DELETE    DELETE    DELETE    DELETE    DELETE    DELETE    DELETE
-                    </Link>
-
                 </header>
             }
         >
@@ -28,7 +20,7 @@ export default function Dashboard({ auth }) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div className="bg-zinc-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
                             Dashboard
                         </div>
