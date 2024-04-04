dfd0df616e286b42bc62e1a8f66ddcf024bfd6dc
BubakVOE <ludwigtom@seznam.cz>
2024-04-04 02:16:31 +0200

    test automation

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -21,7 +21,7 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-zinc-900 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-300">
+                        <div className="p-6 text-gray-200">
                             Dashboard
                         </div>
                     </div>
