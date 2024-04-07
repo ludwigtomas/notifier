2a3956cb033b298229e22cf990c3335f412ae90e
BubakVOE <ludwigtom@seznam.cz>
2024-04-08 00:13:36 +0200

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('gitlab:repositories')->everyTwoDays();
+        $schedule->command('gitlab:repositories')->everyMinute();
     }
 
     /**
