93b3c7b09fa2bbfb90bb9ba7879a9d3978292dc4
BubakVOE <ludwigtom@seznam.cz>
2024-04-07 12:59:58 +0200

    update scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,8 +12,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('gitlab:repositories')->hourly();
-        $schedule->command('gitlab:repositories')->at('15:00');
+        $schedule->command('gitlab:repositories')
+            ->everyMinute()
+            ->withoutOverlapping()
+            ->runInBackground();
     }
 
     /**
@@ -22,7 +24,7 @@ class Kernel extends ConsoleKernel
     protected function commands(): void
     {
 
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
