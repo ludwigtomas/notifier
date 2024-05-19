ebb586ca1f3447a8171e1f4190f12bf9b55bc367
BubakVOE <ludwigtom@seznam.cz>
2024-05-19 15:39:49 +0200

    Save before SSH

--- resources/js/Pages/Repositories/Index.jsx
+++ resources/js/Pages/Repositories/Index.jsx
@@ -272,13 +272,13 @@ export default function Index({ auth, repositories, filters }) {
                                                         { repository.relationships.clients_count > 0 ? (
                                                             <div className="pl-3 py-1 pr-1 flex items-center justify-between space-x-4 rounded-full bg-zinc-800 group-hover:bg-zinc-900 faster-animation">
                                                                 {repository.relationships.clients.slice(0, 2).map((client) => (
-                                                                        <p
-                                                                            key={client.id}
-                                                                            className="text-xs text-zinc-400"
-                                                                        >
-                                                                            {client.name}
-                                                                        </p>
-                                                                    ))}
+                                                                    <p
+                                                                        key={client.id}
+                                                                        className="text-xs text-zinc-400"
+                                                                    >
+                                                                        {client.name}
+                                                                    </p>
+                                                                ))}
 
                                                                 {repository.relationships.clients_count > 2 && (
                                                                     <span className="p-1.5 text-xs text-zinc-400 rounded-full bg-zinc-900 group-hover:bg-zinc-700 faster-animation">
