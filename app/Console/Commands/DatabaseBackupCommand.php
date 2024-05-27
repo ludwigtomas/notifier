b1be307aa10fcf261353fa7e6043fbf6209fd502
BubakVOE <ludwigtom@seznam.cz>
2024-05-27 13:11:53 +0200

    Added backup for notifier

---
+++ app/Console/Commands/DatabaseBackupCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Services\DatabaseService;
+
+class DatabaseBackupCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'database:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $backup_path = DatabaseService::createDatabaseBackup();
+
+        DatabaseService::sendDatabaseBackup($backup_path);
+    }
+}
---
+++ app/Services/DatabaseService.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Services;
+
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class DatabaseService
+{
+    public static function createDatabaseBackup()
+    {
+        Log::channel('backup')->info('⚙️ STARTING NEW BACKUP ⚙️');
+
+        $filename = "backup-" . Carbon::now()->format('Y-m-d') . ".sql";
+
+        Storage::disk('local')->makeDirectory('backups');
+
+        $path = storage_path('app/backups/' . $filename);
+
+        Log::channel('backup')->info('➡️ creating backup file');
+
+        $command = "mysqldump --no-tablespaces --user=" . env('DB_USERNAME') . " --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . " > " . $path;
+
+        exec($command);
+
+        $time_out = 0;
+
+        while ($time_out < 5) {
+            $time_out++;
+            sleep(1);
+        }
+
+        return $path;
+    }
+
+    public static function sendDatabaseBackup($path)
+    {
+        Log::channel('backup')->info('➡️ preparing file for sending');
+
+        try {
+            $client = new Client();
+
+            $response = $client->request('POST', env('BACKUP_URL'), [
+                'multipart' => [
+                    [
+                        'name' => 'backup_file',
+                        'contents' => fopen($path, 'r'),
+                        'filename' => basename($path),
+                    ],
+                    [
+                        'name' => 'password',
+                        'contents' => env('BACKUP_CODE'),
+                    ],
+                ],
+            ]);
+
+            if ($response->getStatusCode() == 200 || $response->getStatusCode() == 201) {
+                Log::channel('backup')->info('➡️ file was sent');
+
+                File::delete($path);
+
+                Log::channel('backup')->info('➡️ file was deleted');
+                Log::channel('backup')->info('✅ END OF BACKUP');
+            }
+        } catch (\Throwable $th) {
+            Log::channel('backup')->critical('➡️ an error occurred while uploading a file', ['th' => $th->getMessage()]);
+            Log::channel('backup')->emergency('❌ END OF SESSION ❌');
+        }
+    }
+}
