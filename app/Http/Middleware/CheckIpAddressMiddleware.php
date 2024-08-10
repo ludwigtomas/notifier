cc81c3181be3c28160362155b339c468f5050d90
bubak <ludwigtom@seznam.cz>
2024-08-10 23:31:40 +0200

    Added middleware for IP address

---
+++ app/Http/Middleware/CheckIpAddressMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckIpAddressMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $allowed_ips = [
+            '109.80.147.80',
+            '127.0.0.1',
+        ];
+
+        if (! in_array($request->ip(), $allowed_ips)) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        return $next($request);
+    }
+}
