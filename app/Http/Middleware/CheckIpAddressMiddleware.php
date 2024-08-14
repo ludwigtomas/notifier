210834876d015572581f04b9638bfc4ecd0afa4b
bubak <ludwigtom@seznam.cz>
2024-08-14 14:43:55 +0200

    Fix CI/CD

--- app/Http/Middleware/CheckIpAddressMiddleware.php
+++ app/Http/Middleware/CheckIpAddressMiddleware.php
@@ -15,19 +15,21 @@ class CheckIpAddressMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (app()->isProduction()) {
-            $allowed_ips = [
-                '192.168.50.210',
-            ];
-
-            if (! in_array($request->ip(), $allowed_ips)) {
-                return response()->json([
-                    'message' => 'Unauthorized',
-                    'ip_address' => $request->ip(),
-                ], 403);
-            }
-
+        if (app()->isLocal()) {
             return $next($request);
+        }
+
+
+        
+        $allowed_ips = [
+            '192.168.50.210',
+        ];
+
+        if (! in_array($request->ip(), $allowed_ips)) {
+            return response()->json([
+                'message' => 'Unauthorized',
+                'ip_address' => $request->ip(),
+            ], 403);
         }
 
         return $next($request);
