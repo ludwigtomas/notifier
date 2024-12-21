7f731a4d4ed40edf34e31dec2cf82252a2f7fbaf
bubak <ludwigtom@seznam.cz>
2024-12-21 15:43:28 +0100

    fix: layout

--- resources/js/Layouts/AdminLayout.jsx
+++ resources/js/Layouts/AdminLayout.jsx
@@ -8,7 +8,7 @@ export default function Authenticated({ user, header, children }) {
             <Sidebar user={user} />
 
             <section className="w-full">
-                {/* {header && (
+                {header && (
                     <header className='fixed top-0 right-0 w-full z-50 pl-[6.6rem] lg:pl-[14rem]'>
                         <div className='className="grid p-6 bg-zinc-900/50 backdrop-blur-md border-b border-neutral-700'>
                             <div className='flex items-center space-x-4 text-neutral-400'>
@@ -16,9 +16,9 @@ export default function Authenticated({ user, header, children }) {
                             </div>
                         </div>
                     </header>
-                )} */}
+                )}
 
-                <Breadcrumbs/>
+                {/* <Breadcrumbs/> */}
 
                 <div className="max-w-[100rem] mt-28 mx-auto pb-10">
                     {children}
