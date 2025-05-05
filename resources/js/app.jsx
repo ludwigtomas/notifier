9eea84390ef3ef916da3fe902231604deb388e5f
Tomas Ludwig <ludwigtom@seznam.cz>
2025-05-05 15:11:28 +0200

    Feat: add sentry for FE + BE

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,13 +1,19 @@
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    tracesSampleRate: 1.0,
+})
+
 import './bootstrap'
 import '../css/app.css'
-
 import React from 'react'
-
 import { createRoot } from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/react'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
+
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -15,7 +21,12 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el)
 
-        root.render(<App {...props} />)
+        root.render(
+            <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
+                <App {...props} />
+            </Sentry.ErrorBoundary>
+        )
+        // root.render(<App {...props} />)
     },
     progress: {
         color: '#00DDFF',
